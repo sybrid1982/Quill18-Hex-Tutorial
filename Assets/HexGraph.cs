1323fb333702cb7fbfe885810dac0279bc470acd
Sydney Bridges <sybrid@gmail.com>
2017-05-28 11:35:26 -0400

    Refactored HexGraph and HexPath
    No actual changes to functionality, just breaking larger methods into smaller ones

--- Assets/HexGraph.cs
+++ Assets/HexGraph.cs
@@ -14,22 +14,39 @@ public class HexGraph {
         //no nodes for unwalkable hexes
         //TODO: Implement existence of non-walkable hexes
 
-        int nodeCount = 0;
-        for(int q=0; q< hexMap.NumColumns(); q++)
-        {
-            for (int r = 0; r < hexMap.NumRows(); r++)
-            {
-                Hex h = hexMap.GetHexFromHexMap(q, r);
-                Path_Node<Hex> n = new Path_Node<Hex>();
-                n.data = h;
-                nodes.Add(h, n);
-                nodeCount++;
-            }
-        }
-        //Debug.Log("Node count: " + nodeCount);
+        CreateNodes(hexMap);
 
+        CreateEdges();
+
+    }
+
+    private void CreateNodes(HexMap hexMap)
+    {
+        int nodeCount = 0;
+        for (int q = 0; q < hexMap.NumColumns(); q++)
+        {
+            nodeCount += CycleThroughRowToMakeNodes(hexMap, nodeCount, q);
+        }
+    }
+
+    private int CycleThroughRowToMakeNodes(HexMap hexMap, int nodeCount, int q)
+    {
+        for (int r = 0; r < hexMap.NumRows(); r++)
+        {
+            Hex h = hexMap.GetHexFromHexMap(q, r);
+            Path_Node<Hex> n = new Path_Node<Hex>();
+            n.data = h;
+            nodes.Add(h, n);
+            nodeCount++;
+        }
+
+        return nodeCount;
+    }
+
+    private void CreateEdges()
+    {
         int edgeCount = 0;
-        foreach(Hex h in nodes.Keys)
+        foreach (Hex h in nodes.Keys)
         {
             Path_Node<Hex> n = nodes[h];
             //get a list of neighbors for the hexes
@@ -38,23 +55,28 @@ public class HexGraph {
             List<Path_Edge<Hex>> edges = new List<Path_Edge<Hex>>();
             Hex[] neighbors = h.GetNeighbors();
 
-            for (int i = 0; i < neighbors.Length; i++)
-            {
-                if(neighbors[i]!=null && neighbors[i].MovementCost > 0)
-                {
-                    Path_Edge<Hex> e = new Path_Edge<Hex>();
-                    e.cost = neighbors[i].MovementCost;
-                    e.node = nodes[neighbors[i]];
-
-                    //add the edge to the temporary and growable list
-                    edges.Add(e);
-                    edgeCount++;
-                }
-            }
+            edgeCount += CreateEdgesFromNeighbors(edgeCount, edges, neighbors);
 
             n.edges = edges.ToArray();
         }
+    }
 
-        //Debug.Log("Edge count: " + edgeCount);
+    private int CreateEdgesFromNeighbors(int edgeCount, List<Path_Edge<Hex>> edges, Hex[] neighbors)
+    {
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            if (neighbors[i] != null && neighbors[i].MovementCost > 0)
+            {
+                Path_Edge<Hex> e = new Path_Edge<Hex>();
+                e.cost = neighbors[i].MovementCost;
+                e.node = nodes[neighbors[i]];
+
+                //add the edge to the temporary and growable list
+                edges.Add(e);
+                edgeCount++;
+            }
+        }
+
+        return edgeCount;
     }
 }
--- Assets/HexPathfinding.cs
+++ Assets/HexPathfinding.cs
@@ -11,7 +11,7 @@ public class HexPath {
     {
         hexStack = new Stack<Hex>();
 
-        if(hexMap.hexGraph == null)
+        if (hexMap.hexGraph == null)
         {
             hexMap.hexGraph = new HexGraph(hexMap);
         }
@@ -22,15 +22,8 @@ public class HexPath {
         //haven't yet evaluated
         Dictionary<Hex, Path_Node<Hex>> nodes = hexMap.hexGraph.nodes;
 
-        //Check if the hex is impassible
-        if(nodes.ContainsKey(end) == false)
-        {
-            Debug.Log("Impassible hex cannot be entered");
-            return;
-        }
-
         List<Path_Node<Hex>> closedSet = new List<Path_Node<Hex>>(); ;
-        
+
         SimplePriorityQueue<Path_Node<Hex>> openSet = new SimplePriorityQueue<Path_Node<Hex>>();
         openSet.Enqueue(nodes[start], 0);
 
@@ -39,15 +32,16 @@ public class HexPath {
         Dictionary<Path_Node<Hex>, float> gScore = new Dictionary<Path_Node<Hex>, float>();
         Dictionary<Path_Node<Hex>, float> fScore = new Dictionary<Path_Node<Hex>, float>();
 
-        //initialize all gScore and fScore values to 'infinity'
-        foreach (Path_Node<Hex> n in nodes.Values)
+        //Check if the hex is impassible
+        if (nodes.ContainsKey(end) == false)
         {
-            gScore[n] = Mathf.Infinity;
-            fScore[n] = Mathf.Infinity;
+            Debug.Log("Impassible hex cannot be entered");
+            return;
         }
-        //intialize the values for the starting node
-        gScore[nodes[start]] = 0;
-        fScore[nodes[start]] = bestPossiblefScore(start, end);
+
+        //initialize all gScore and fScore values to 'infinity'
+        //Also initialize the gScore and fScore of the starting hex
+        InitializeScores(start, end, nodes, gScore, fScore);
 
         while (openSet.Count > 0)
         {
@@ -57,7 +51,6 @@ public class HexPath {
             //if the top of the queue is the end hex, we're done looking
             if (current.data == end)
             {
-                //Debug.Log("HexPathfinding: Valid path found!");
                 hexStack = ReconstructPath(cameFrom, current);
 
                 return;
@@ -66,38 +59,55 @@ public class HexPath {
             closedSet.Add(current);
 
             //Look at current's neighbors
-            foreach (Path_Edge<Hex> e in current.edges)
-            {
-                //if we already evaluated this hex, abort
-                if (closedSet.Contains(e.node))
-                {
-                    continue;
-                }
-
-                float tentative_gScore = gScore[current] + e.cost;
-
-                //If we've already a path that gets to this edge which is faster, abort
-                if(openSet.Contains(e.node) && tentative_gScore >= gScore[e.node])
-                {
-                    continue;
-                }
-
-                //Record this path as it is currently the best
-                cameFrom[e.node] = current;
-                gScore[e.node] = tentative_gScore;
-                fScore[e.node] = gScore[e.node] + bestPossiblefScore(e.node.data, end);
-                if (openSet.Contains(e.node) == false)
-                {
-                    //then we've found a path to a new hex and need to add it to the open set
-                    openSet.Enqueue(e.node, fScore[e.node]);
-                } else
-                {
-                    //Otherwise this is merely a faster way to a hex we've already had and can update its score
-                    openSet.UpdatePriority(e.node, fScore[e.node]);
-                }
-            }
+            EvaluateNeighbors(end, closedSet, openSet, cameFrom, gScore, fScore, current);
             //If we get here then we failed to find a path
-            //Debug.Log("HexPathfinding: No path found!");
+        }
+    }
+
+    private void InitializeScores(Hex start, Hex end, Dictionary<Hex, Path_Node<Hex>> nodes, Dictionary<Path_Node<Hex>, float> gScore, Dictionary<Path_Node<Hex>, float> fScore)
+    {
+        foreach (Path_Node<Hex> n in nodes.Values)
+        {
+            gScore[n] = Mathf.Infinity;
+            fScore[n] = Mathf.Infinity;
+        }
+        //intialize the values for the starting node
+        gScore[nodes[start]] = 0;
+        fScore[nodes[start]] = bestPossiblefScore(start, end);
+    }
+
+    private void EvaluateNeighbors(Hex end, List<Path_Node<Hex>> closedSet, SimplePriorityQueue<Path_Node<Hex>> openSet, Dictionary<Path_Node<Hex>, Path_Node<Hex>> cameFrom, Dictionary<Path_Node<Hex>, float> gScore, Dictionary<Path_Node<Hex>, float> fScore, Path_Node<Hex> current)
+    {
+        foreach (Path_Edge<Hex> e in current.edges)
+        {
+            //if we already evaluated this hex, abort
+            if (closedSet.Contains(e.node))
+            {
+                continue;
+            }
+
+            float tentative_gScore = gScore[current] + e.cost;
+
+            //If we've already a path that gets to this edge which is faster, abort
+            if (openSet.Contains(e.node) && tentative_gScore >= gScore[e.node])
+            {
+                continue;
+            }
+
+            //Record this path as it is currently the best
+            cameFrom[e.node] = current;
+            gScore[e.node] = tentative_gScore;
+            fScore[e.node] = gScore[e.node] + bestPossiblefScore(e.node.data, end);
+            if (openSet.Contains(e.node))
+            {
+                //This is a faster way to a hex we've already had and can update its score
+                openSet.UpdatePriority(e.node, fScore[e.node]);
+            }
+            else
+            {
+                //We've found a path to a new hex and need to add it to the open set
+                openSet.Enqueue(e.node, fScore[e.node]);
+            }
         }
     }
 
@@ -143,8 +153,6 @@ public class HexPath {
     public Stack<Hex> ClonePathStack()
     {
         var clone = new Stack<Hex>(new Stack<Hex>(hexStack));
-        //Debug.Log("HexStack length: " + hexStack.Count);
-        //Debug.Log("Clone length: " + clone.Count);
         return clone;
     }
 }
